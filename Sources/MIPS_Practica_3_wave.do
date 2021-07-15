6a617e7917aef8df308e44bbe8893f5507142efb
EdgarHdz-13 <85575704+EdgarHdz-13@users.noreply.github.com>
2021-07-14 22:51:15 -0500

    últimos detalles
    simplemente unos detalles menores

---
+++ Sources/MIPS_Practica_3_wave.do
@@ -0,0 +1,5 @@
+N_BITS = 5
+selector_i = St0
+data_0_i = xxxxx
+data_1_i = xxxxx
+mux_o = xxxxx
--- Sources/Test_Program_Project_3_ Hazards.asm
+++ Sources/Test_Program_Project_3_ Hazards.asm
@@ -1,21 +1,7 @@
 
-#Forwarding aritmetico-lógico
-
-#addi 	$t1,$zero,10
-#add  	$t2,$zero,$t1
-
-#Forwarding Branch
-jal	funcion_1
-
 addi 	$t3,$zero,100
-j exit
-funcion_1:
-addi	$t3,$zero,1
-addi	$t3,$zero,2
-addi	$t3,$zero,3
-jr	$ra
-
-addi $t1,$zero,5
-exit:
-
+addi 	$sp,$sp,-4
+sw 	$t3,0($sp)
+lw 	$t3,0($sp)
+add 	$t1,$zero,$t3
 
